b31da3ea509b9a97389fad3d06eac508efceff25
amritsrivastava <srivastava.amrit1999@gmail.com>
2019-12-27 20:39:43 +0530

    setup passport facebook authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/conFusion'
+  mongoUrl: 'mongodb://localhost:27017/conFusion',
+  facebook: {
+    clientId: '2662934237261526',
+    clientSecret: '8b0f2d2ce210e5a5829b5d29841a110d'
+  }
 };
